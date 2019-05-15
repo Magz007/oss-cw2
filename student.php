f7d141d242bda929c87b8993fe75f05e6a7a0bf1
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-16 00:10:38 +0100

    new

--- student.php
+++ student.php
@@ -18,13 +18,24 @@
 
       // prepare page content
       $data['content'] .= "<table border='1'>";
-      $data['content'] .= "<tr><th colspan='5' align='center'>Modules</th></tr>";
-      $data['content'] .= "<tr><th>Code</th><th>Type</th><th>Level</th></tr>";
+      $data['content'] .= "<tr><th colspan='5' align='center'>Student Records</th></tr>";
+      $data['content'] .= "<tr>
+      <th>Student ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Last Name</th>
+      <th>Last Name</th>
+      <th>Last Name</th>
+      <th>Last Name</th>
+      <th>Last Name</th>
+      <th>Last Name</th>
+      </tr>";
+
 
       // Display the modules within the html table
       while($row = mysqli_fetch_array($result)) {
-         $data['content'] .= "<tr><td> $row[modulecode] </td><td> $row[name] </td>";
-         $data['content'] .= "<td> $row[level] </td></tr>";
+         $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td><td> $row[firstname] </td><td> $row[lastname] </td>";
+
       }
       $data['content'] .= "</table>";
 
