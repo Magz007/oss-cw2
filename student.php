c84644d7efffcf4e7e2a3caf74fe285eb5028e79
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-17 20:15:54 +0100

    save

--- student.php
+++ student.php
@@ -56,7 +56,7 @@
                    <td><?php echo $row['country'] ;?> </td>
                     <td><?php echo $row['postcode'] ;?> </td>
                      <td> <input type= "checkbox" name= "btndelete" value=<?php echo $row['studentid'] ;?> required></td>
-                      <td> <input type= "submit" name="submitDeleteBtn" class-= "btn btn-info">  </td>
+                      <td> <input type= "Delete" name="submitDeleteBtn" class-= "btn btn-info">  </td>
      </form>
 
     </tr>
