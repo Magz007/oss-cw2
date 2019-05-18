6e5a4af53c0664e63cb3bda6015501cb44cc430f
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-18 14:46:20 +0100

    NEW

--- addstudent.php
+++ addstudent.php
@@ -23,14 +23,14 @@ $con = mysqli_connect("localhost", "root", "", "");
 <form action="" method= "post" role = "form">
 <br>
 
-FirstName:    <input type= "text" value="fristname"> <br><br>
-Last Name:    <input type= "text" value="lastname"> <br><br>
-Date of Birth:<input type= "text" value="dob"> <br><br>
-Address:      <input type= "text" value="house"> <br><br>
-Town :        <input type= "text" value="town"> <br><br>
-County:       <input type= "text" value="county"> <br><br>
-Conutry:      <input type= "text" value="counrty"> <br><br>
-Post Code:     <input type= "text" value="postCode"> <br><br>
+FirstName:    <input type= "text" name="fristname"> <br><br>
+Last Name:    <input type= "text" name="lastname"> <br><br>
+Date of Birth:<input type= "text"name="dob"> <br><br>
+Address:      <input type= "text" name="house"> <br><br>
+Town :        <input type= "text" name="town"> <br><br>
+County:       <input type= "text" name="county"> <br><br>
+Conutry:      <input type= "text" name="counrty"> <br><br>
+Post Code:     <input type="text"name="postCode"> <br><br>
 
 <input type= "submit" value= "insert">
 
