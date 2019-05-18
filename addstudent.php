08cbc55225bac1d9c8cde29cf19005636a4d8711
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-18 02:48:16 +0100

    saved

--- addstudent.php
+++ addstudent.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['id'])) {
    echo template("templates/partials/header.php");
    echo template("templates/partials/nav.php");
 
-  $db_seclected= mysqli_select_db(student,include('_includes/dbconnect.inc')); 
+  $db_seclected= mysqli_select_db('student',include('_includes/dbconnect.inc'));
   if(!$db_seclected)
   {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
