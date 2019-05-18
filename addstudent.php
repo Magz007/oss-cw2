b023772fe2f2c27ad864d7a4fcd987c0535c50b6
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-18 13:01:48 +0100

    new

--- addstudent.php
+++ addstudent.php
@@ -23,10 +23,10 @@ $con = mysqli_connect("localhost", "root", "", "");
 <form action="" method= "post" role = "form">
 <br>
 
-First Name  :<input type= "text" name="fristname"> <br><br>
+FirstName  :<input type= "text" name="fristname"> <br><br>
 Last Name:   <input type= "text" name="lastname"> <br><br>
 Date of Birth:<input type= "float" name="dob"> <br><br>
-1st Line Address:<input type= "text" name="1stLineAddress"> <br><br>
+Address:<input type= "text" name="1stLineAddress"> <br><br>
 Town :<input type= "text" name="Town"> <br><br>
 County: <input type= "text" name="County"> <br><br>
 Conutry: <input type= "text" name="Counrty"> <br><br>
@@ -42,8 +42,8 @@ Post Code: <input type= "text" name="PostCode"> <br><br>
    die();
  }
 
-$fristname = $_POST['fristname'];
-$lastname= $_POST['lastname'];
+$FirstName = $_POST['fristname'];
+$= $_POST['lastname'];
 $dob= $_POST['dob'];
 $lineaddress= $_POST['1stLineAddress'];
 $town= $_POST['Town'];
@@ -52,7 +52,7 @@ $country= $_POST['Country'];
 $postcode= $_POST['Postcode'];
 
 $sql = "insert into student ('firstname','lastname','dob','house','town','county','country','postcode')
-values ('$fristname','$lastname','$dob','$lineaddress','$town','$county','$conutry','$postcode')";
+values ('$FirstName','$lastname','$dob','$lineaddress','$town','$county','$conutry','$postcode')";
 
 
 
