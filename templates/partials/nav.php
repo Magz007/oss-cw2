3922a7219124324eac07cd183f573e87b519b927
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-19 11:25:46 +0100

    new

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -5,7 +5,7 @@
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
    <a href="student.php">Existing Student</a> |
-    <a href="addstudent.html">Add New Student</a> |
+    <a href="addstudent.php">Add New Student</a> |
    <a href="logout.php">Logout</a>
 
 </div>
