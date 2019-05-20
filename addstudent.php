dfbfd44951084a379be78d3d827e89b9697aa89b
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-20 09:51:56 +0100

    new

--- addstudent.php
+++ addstudent.php
@@ -17,33 +17,24 @@ if (isset($_SESSION['id']))
 <form action="" method= "post">
 <br>
 
-FirstName:    <input type= "text" name="fristname"> <br><br>
-Last Name:    <input type= "text" name="lastname">  <br><br>
-Date of Birth:<input type= "text" name="dob">       <br><br>
-Address:      <input type= "text" name="house">     <br><br>
-Town :        <input type= "text" name="town">      <br><br>
-County:       <input type= "text" name="county">    <br><br>
-Conutry:      <input type= "text" name="country">   <br><br>
-Post Code:    <input type= "text" name="postcode">  <br><br>
+FirstName:    <input type= "text" name=<?php $_POST['fristname'];?>> <br><br>
+Last Name:    <input type= "text" name=<?php $_POST['lastname'];?>> <br><br>
+Date of Birth:<input type= "text" name=<?php $_POST['dob'];?>>   <br><br>
+Address:      <input type= "text" name=<?php $_POST['house'];?>>  <br><br>
+Town :        <input type= "text" name=<?php $_POST['town'];?>> <br><br>
+County:       <input type= "text" name=<?php $_POST['county'];?>>  <br><br>
+Conutry:      <input type= "text" name=<?php $_POST['country'];?>>  <br><br>
+Post Code:    <input type= "text" name=<?php $_POST['postcode'];?>>  <br><br>
              <button type= "submit" name="submit" value="SUBMIT" class= "btn btn-info"></button>
 </form>
 
 <?php
 
 
-/* $fristname=$_POST['fristname'];
-  $lastname=$_POST['lastname'];
-    $dob= $_POST['dob'];
-    $house= $_POST['house'];
-     $town= $_POST['town'];
-      $county= $_POST['county'];
-        $country= $_POST['country'];
-          $postcode= $_POST['postcode'];*/
-
 if (isset($_POST['submit']))
 {
 $sql = "insert into student(dob,firstname,lastname,house,town,county,country,postcode)
- values('$dob','$fristname','$lastname','$house', '$town', ' $county', '$country' ,'$postcode');";
+ values('firstname','lastname','dob','house, 'town', ' county', 'country' ,'postcode');";
 
 mysqli_query($conn, $sql);
 header("Location: index.php?addstudent=success");
