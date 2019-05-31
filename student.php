3b6eefc83b818e33ba330a3a5592372218b830c7
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-31 02:20:52 +0100

    new

--- student.php
+++ student.php
@@ -72,7 +72,7 @@
     {
         $delete=$_POST['records'];
 
-        mysqli_query ("Delete * from student where studentid='". $delete . "';");
+        mysqli_query ("Delete * from student where studentid = $delete");
         //ref:https://www.w3schools.com/php/php_mysql_insert.asp
        if (mysqli_query($conn,$sql))
        {
