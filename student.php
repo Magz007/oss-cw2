e227cce8c64c5699d67f535fb51c1cd5ac80f9a8
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-05-31 11:18:53 +0100

    new

--- student.php
+++ student.php
@@ -15,7 +15,6 @@
       $sql= ("SELECT * from student  where studentid = '" . $_SESSION['id'] ."';");
       $result = mysqli_query($conn,$sql);
 
-
 $x = 1;
 
 while($row = mysqli_fetch_row($result))
@@ -23,6 +22,8 @@ while($row = mysqli_fetch_row($result))
     echo  $row[1] ;
     $x++;
 }
+mysqli_free_result($result);
+mysqli_close($conn);
 
 ?>
       <html>
