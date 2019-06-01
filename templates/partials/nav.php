8f3b0b68249bea6e33ee88da8c173f0b0d29a832
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-01 02:05:15 +0100

    new

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -8,7 +8,7 @@ include("js/index.html");
    <a href="modules.php">My Modules</a> |
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
-   <a href="student.php">Existing Student</a> |
+   <a href="student.html">Existing Student</a> |
     <a href="addstudent.html">Add New Student</a> |
    <a href="logout.php">Logout</a>
 
