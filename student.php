2a5fdd909363d3808a5372563a9040e8ff954698
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-01 03:09:57 +0100

    new

--- student.php
+++ student.php
@@ -12,14 +12,14 @@ if (isset($_SESSION['id']))
       echo template("templates/partials/nav.php");
 // Build SQL statment that selects a student's database
 // Build sql statment that selects all the modules
-$sql = "select * from student" ;
+$sql = "select * from student where studentid='". $_SESSION['id'] . "'; ";
 $result = mysqli_query($conn, $sql);
 $checkresult=mysqli_num_rows($result);
 if($checkresult>0)
 {
   while($row= mysqli_fetch_assoc($result))
   {
-    echo $row['studentid']. "<br>";
+    echo  $_SESSION['id']. "<br>";
   }
 }
 
