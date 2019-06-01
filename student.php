e9f3a0cb29dd0c5c2369a8538302b2548e04a474
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-01 03:42:41 +0100

    new

--- student.php
+++ student.php
@@ -20,6 +20,19 @@ if($checkresult>0)
   while($row= mysqli_fetch_assoc($result))
   {?>
 <table class='table' table border= "1px" >
+  <tr>
+    <th>Number</th>
+    <th>Password</th>
+     <th>D.O.B</th>
+      <th>First Name</th>
+       <th>Last Name</th>
+         <th>1st Line Address</th>
+          <th>Town</th>
+           <th>County</th>
+            <th>Country</th>
+             <th>Post Code</th>
+
+         </tr>
     <tr>
            <td><?php echo $row['studentid'];?> <td>
             <td><?php echo $row['password'] ;?> </td>
