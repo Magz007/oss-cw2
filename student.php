3a665302e742a653585b8ac28b46306ca0f7b03f
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-01 10:39:14 +0100

    new

--- student.php
+++ student.php
@@ -40,6 +40,7 @@ $checkresult=mysqli_num_rows($result);
 </thead>
       <tbody>
     <tr>
+        <td><?php echo $row[$sr];?> </td>
       <td><?php echo $row['studentid'];?> </td>
        <td><?php echo $row['dob'] ;?> </td>
        <td><?php echo $row['firstname'];?> </td>
@@ -56,9 +57,10 @@ $checkresult=mysqli_num_rows($result);
 
 </table>
 </div>
-}
-<?php
 
+<?php
+}
+$sr ++;
 
 
 }
