8e1a86803020174033e87126c784c39a55d564e5
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-01 12:28:17 +0100

    new

--- student.php
+++ student.php
@@ -45,7 +45,7 @@ while ($row= mysqli_fetch_array($result))
   <td>$row[country]</td>
   <td>$row[postcode]</td>
   <td>$row[]</td>
-  <td> <input type="checkbox" name="checkbox" value=$row[studentid] </td>
+  <td> <input type="checkbox" name="checkbox" value=$row[record] </td>
 
 
   </tr>";
