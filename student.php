ba753b9b6742122bb7576afd53259bc7b4c5c6b5
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-01 02:56:27 +0100

    new

--- student.php
+++ student.php
@@ -14,43 +14,9 @@ if (isset($_SESSION['id']))
 // Build sql statment that selects all the modules
 $sql = "select * from student where studentid='". $_SESSION['id'] . "';";
 $result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result
+echo $row=['studentid'].'<br>';
 
-$rn=1;
-// Display the mstudent details
-while($row = mysqli_fetch_array($result)<0)
-{?>
-     <form action="addstudent.php" method= "post" role = "form">
-    <tr>
-          <td><?php echo $row[$rn] ;?> </td>
-            <td><?php echo $row['password'] ;?> </td>
-             <td><?php echo $row['dob'] ;?> </td>
-              <td><?php echo $row['firstname'] ;?> </td>
-               <td><?php echo $row['lastname'] ;?> </td>
-                <td><?php echo $row['house'] ;?> </td>
-                 <td><?php echo $row['town'] ;?> </td>
-                  <td><?php echo $row['county'] ;?> </td>
-                   <td><?php echo $row['country'] ;?> </td>
-                    <td><?php echo $row['postcode'] ;?> </td>
-                     <td> <input type= "checkbox" name= "records[]" value=<?php echo $row['studentid'] ;?> required></td>
-                      <td> <input type="submit" name="submit" value="DELETE"></td>
-  </form>
-     </tr>
-<?php $rn ++;
-
-    if (isset($_POST['submit']))
-      {
-     $i=0;
-     $keyToDelete=$_row['studentid[]'];
-          while ($i<$keyToDelete)
-          {
-         $sql = "Delete * from student where studentid='". $_SESSION['id'] . "';";
-         $result = mysqli_query($conn,$sql);
-         $row = mysqli_fetch_array($result);
-
-         $i++;
-          }
-      }
-}
 }
 else
 {
