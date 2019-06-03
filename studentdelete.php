ce725cec627d51fa8aa9e5cb6d53b55c71a8c4f9
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-03 12:01:38 +0100

    new

--- studentdelete.php
+++ studentdelete.php
@@ -3,7 +3,7 @@
 include("_includes/config.inc");
 include("_includes/dbconnect.inc");
 include("_includes/functions.inc");
-// include("css/index.html");
+include("css/index.html");
 // check logged in
 if (isset($_SESSION['id']))
 {
@@ -12,55 +12,68 @@ if (isset($_SESSION['id']))
 // Build SQL statment that selects a student's database
 // Build sql statment that selects all the modules
 ?>
+
 <html>
 <head> Delete Record </head>
 <body style="padding-top: 100px;" >
 <div class= "container">
 <table class='table' table border= "5px">
-<?php
-$sql = "select * from student; ";
-$result = mysqli_query($conn, $sql);
-?>
+
+<thread>
+
 <tr>
 <th>#</th>
 <th>Student ID</th><th>D.O.B</th><th>First Name</th><th>Last Name</th><th>1st Line Address</th><th>Town</th><th>County</th>
 <th>Counrty</th><th>Post Code</th><th>Check</th><th>Submit </th>
 </tr>
+</thread>
+
+<tbody>
 <?php
+$sql = "select * from student; ";
+$result = mysqli_query($conn, $sql);
 $sr=1;
-while($row= mysqli_fetch_assoc($result))
-{  ?>
+while($row= mysqli_fetch_assoc($result)){
+?>
     <tr>
-    <form action="" method= "post" role = "form">
-      <td><?php echo $sr ;?> </td>      <td><?php echo $row['studentid'] ;?> </td>   <td><?php echo $row['dob'] ;?> </td>
+
+      <td><?php echo $sr ;?> </td>    <td><?php echo $row['dob'] ;?> </td>
       <td><?php echo $row['firstname'] ;?> </td> <td><?php echo $row['lastname'] ;?> </td> <td><?php echo $row['house'] ;?> </td>
       <td><?php echo $row['town'] ;?> </td>  <td><?php echo $row['county'] ;?> </td>  <td><?php echo $row['country'] ;?> </td>
       <td><?php echo $row['postcode'] ;?> </td>
 
-      <td> <input type= "checkbox" name= "checkbox" value=<?php echo $row['studentid'] ;?> required></td>
-      <td> <input type= "submit" name="submit" value="Delete" class-= "btn btn-info">  </td>
-     </form>
-</tr>
-<?php
- $sr ++ ;
- $data['content'] .= "</table>";
-}
-?>
-<?php
-    if (isset($_POST['submit']))
-      {
-      $checkbox=$row['studentid'];
-      $sql = "Delete * from student where studentid='".$checkbox . "'; ";
-      $result=mysqli_query($conn,$sql);
-      }
-}
-else
-{
-  header("Location: index.php");
-}
-echo template("templates/partials/footer.php");
+      <form action="" method="post">
+      <td> <input type= "checkbox" name= "checkbox[]" value=<?php echo $row['studentid'] ;?>></td>
 
-?>
+</tr>
+<?php $sr ++ ;}?>
+<div class="row">
+  <div class="form-group">
+    <input type="submit" name="delete"value="DELETE" class="btn btn info">
+  </div>
+</div>
+</form>
+
+<?php
+if (isset($_POST['delete']))
+{
+ $checkedcheckbox =count ($_POST['checkbox']);
+ $i=0;
+ while($i<$checkedcheckbox)
+ {
+   $key=$_POST['checkbox'][$i];
+   $sql= "DELETE from student  where studentid ='$key';";
+   $i++;
+ }
+header("Location: studentdelete.php");
+echo template("templates/partials/footer.php");
+}
+}
+ ?>
+</tbody>
+</table>
+
+
 </table>
 </div>
 </body>
