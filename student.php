73f87989de4be77bd49dfd46e446e4699eb2b768
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-03 10:34:59 +0100

    new

--- student.php
+++ student.php
@@ -1,66 +1,60 @@
 <?php
 
-   include("_includes/config.inc");
-   include("_includes/dbconnect.inc");
-   include("_includes/functions.inc");
-  // include("css/index.html");
-
-   // check logged in
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
+// include("css/index.html");
+// check logged in
 if (isset($_SESSION['id']))
 {
-      echo template("templates/partials/header.php");
-      echo template("templates/partials/nav.php");
+   echo template("templates/partials/header.php");
+   echo template("templates/partials/nav.php");
 // Build SQL statment that selects a student's database
 // Build sql statment that selects all the modules
-
-
+?>
+<html>
+<head> Delete Record </head>
+<body style="padding-top: 100px;" >
+<div class= "container">
+<table class='table' table border= "5px">
+<?php
 $sql = "select * from student; ";
 $result = mysqli_query($conn, $sql);
-
-$data['content'] .= "<table  class='table' table border= '2px'>";
-$data['content'] .= "<tr>
-<th>Student ID</th>
-<th>D.O.B</th>
-<th>First Name</th>
-<th>Last Name</th>
-<th>1st Line Address</th>
-<th>Town</th>
-<th>County</th>
-<th>Counrty</th>
-<th>Post Code</th>
-
-</tr>";
-
-while ($row= mysqli_fetch_array($result))
-{
-
-  $data['content'] .= "<tr>
-  <td>$row[studentid]</td>
-  <td>$row[dob]</td>
-  <td>$row[firstname]</td>
-  <td>$row[lastname]</td>
-  <td>$row[house]</td>
-  <td>$row[town]</td>
-  <td>$row[county]</td>
-  <td>$row[country]</td>
-  <td>$row[postcode]</td>
+?>
+<tr>
+<th>#</th>
+<th>Student ID</th><th>D.O.B</th><th>First Name</th><th>Last Name</th><th>1st Line Address</th><th>Town</th><th>County</th>
+<th>Counrty</th><th>Post Code</th><th>Check</th><th>Submit </th>
+</tr>
+<?php
+$sr=1;
+while($row= mysqli_fetch_assoc($result))
+{  ?>
+    <tr>
+    <form action="" method= "post" role = "form">
+      <td><?php echo $sr ;?> </td>      <td><?php echo $row['studentid'] ;?> </td>   <td><?php echo $row['dob'] ;?> </td>
+      <td><?php echo $row['firstname'] ;?> </td> <td><?php echo $row['lastname'] ;?> </td> <td><?php echo $row['house'] ;?> </td>
+      <td><?php echo $row['town'] ;?> </td>  <td><?php echo $row['county'] ;?> </td>  <td><?php echo $row['country'] ;?> </td>
+      <td><?php echo $row['postcode'] ;?> </td>
 
 
-  </tr>";
+</tr>
+<?php
+ $sr ++ ;
+ $data['content'] .= "</table>";
 }
-  $data['content'] .= "</table>";
-
-
-
-
-  echo template("templates/default.php", $data);
-
- }
- else
- {
+?>
+<?php
+    
+}
+else
+{
   header("Location: index.php");
- }
+}
+echo template("templates/partials/footer.php");
 
- echo template("templates/partials/footer.php");
-
- ?>
+?>
+</table>
+</div>
+</body>
+</html>
--- studentdelete.php
+++ studentdelete.php
@@ -4,7 +4,6 @@ include("_includes/config.inc");
 include("_includes/dbconnect.inc");
 include("_includes/functions.inc");
 // include("css/index.html");
-
 // check logged in
 if (isset($_SESSION['id']))
 {
@@ -18,42 +17,26 @@ if (isset($_SESSION['id']))
 <body style="padding-top: 100px;" >
 <div class= "container">
 <table class='table' table border= "5px">
-  <?php
+<?php
 $sql = "select * from student; ";
 $result = mysqli_query($conn, $sql);
-
 ?>
 <tr>
 <th>#</th>
-<th>Student ID</th>
-<th>D.O.B</th>
-<th>First Name</th>
-<th>Last Name</th>
-<th>1st Line Address</th>
-<th>Town</th>
-<th>County</th>
-<th>Counrty</th>
-<th>Post Code</th>
-<th>Check</th>
-<th>Submit </th>
+<th>Student ID</th><th>D.O.B</th><th>First Name</th><th>Last Name</th><th>1st Line Address</th><th>Town</th><th>County</th>
+<th>Counrty</th><th>Post Code</th><th>Check</th><th>Submit </th>
 </tr>
-
 <?php
 $sr=1;
 while($row= mysqli_fetch_assoc($result))
 {  ?>
     <tr>
     <form action="" method= "post" role = "form">
-      <td><?php echo $sr ;?> </td>
-      <td><?php echo $row['studentid'] ;?> </td>
-      <td><?php echo $row['dob'] ;?> </td>
-      <td><?php echo $row['firstname'] ;?> </td>
-      <td><?php echo $row['lastname'] ;?> </td>
-      <td><?php echo $row['house'] ;?> </td>
-      <td><?php echo $row['town'] ;?> </td>
-      <td><?php echo $row['county'] ;?> </td>
-      <td><?php echo $row['country'] ;?> </td>
+      <td><?php echo $sr ;?> </td>      <td><?php echo $row['studentid'] ;?> </td>   <td><?php echo $row['dob'] ;?> </td>
+      <td><?php echo $row['firstname'] ;?> </td> <td><?php echo $row['lastname'] ;?> </td> <td><?php echo $row['house'] ;?> </td>
+      <td><?php echo $row['town'] ;?> </td>  <td><?php echo $row['county'] ;?> </td>  <td><?php echo $row['country'] ;?> </td>
       <td><?php echo $row['postcode'] ;?> </td>
+
       <td> <input type= "checkbox" name= "checkbox" value=<?php echo $row['studentid'] ;?> required></td>
       <td> <input type= "submit" name="submit" value="Delete" class-= "btn btn-info">  </td>
      </form>
@@ -69,14 +52,12 @@ while($row= mysqli_fetch_assoc($result))
       $checkbox=$row['studentid'];
       $sql = "Delete * from student where studentid='".$checkbox . "'; ";
       $result=mysqli_query($conn,$sql);
-      
       }
 }
 else
 {
   header("Location: index.php");
 }
-
 echo template("templates/partials/footer.php");
 
 ?>
