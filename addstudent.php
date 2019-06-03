d1cf4501ff1728db6cd99d61f14dfe62eed784d8
Margaret Sifuna <Margaret.msifuna@outlook.com>
2019-06-03 12:19:59 +0100

    new

--- addstudent.php
+++ addstudent.php
@@ -10,19 +10,19 @@ if (isset($_SESSION['id']))
   echo template("templates/partials/header.php");
   echo template("templates/partials/nav.php");
 
-$studentid= mysqli_real_escape_string ($conn, $_POST['studentid'];
- $Password= mysqli_real_escape_string ($conn, $_POST['password'];
- $dob=mysqli_real_escape_string ($conn, $_POST['dob'];
- $firstname= mysqli_real_escape_string ($conn, $_POST['firstname'];
- $lastname= mysqli_real_escape_string ($conn, $_POST['lastname'];
- $house=mysqli_real_escape_string ($conn,  $_POST['house'];
- $town= mysqli_real_escape_string ($conn, $_POST['town'];
- $county= mysqli_real_escape_string ($conn,  $_POST['county'];
- $country= mysqli_real_escape_string ($conn, $_POST['country'];
- $postcode=mysqli_real_escape_string ($conn,  $_POST['postcode'];
 
-$sql= "INSERT INTO student (studentid,password,dob,firstname,lastname, house, town, county, country, postcode)
-Values('$studentid','$Password','$dob',' $firstname','$lastname','$house','$town','$county',' $country','$postcode');";
+ $Password=  ($conn, $_POST['password'];
+ $dob=($conn, $_POST['dob'];
+ $firstname=($conn, $_POST['firstname'];
+ $lastname= ($conn, $_POST['lastname'];
+ $house=($conn,  $_POST['house'];
+ $town= ($conn, $_POST['town'];
+ $county=  ($conn,  $_POST['county'];
+ $country=  ($conn, $_POST['country'];
+ $postcode=($conn,  $_POST['postcode'];
+
+$sql= "INSERT INTO student (password,dob,firstname,lastname, house, town, county, country, postcode)
+Values('$Password','$dob',' $firstname','$lastname','$house','$town','$county',' $country','$postcode');";
  //ref:https://www.w3schools.com/php/php_mysql_insert.asp
 
  if(mysqli_query($conn,$sql))
